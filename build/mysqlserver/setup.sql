f98b57e423437515f5e344d9f3332891f7a53842
Martyn Gigg <martyn.gigg@gmail.com>
2024-01-26 13:45:47 +0000

    Pull in DW example for AdventureWorks

--- build/mysqlserver/setup.sql
+++ build/mysqlserver/setup.sql
@@ -14,3 +14,14 @@ WITH
     NOUNLOAD,
     STATS = 5;
 GO
+
+GO
+RESTORE DATABASE [AdventureWorksDW2017]
+FROM DISK = '/backup/AdventureWorksDW2017.bak'
+WITH
+    MOVE 'AdventureWorksDW2017' TO '/backup/AdventureWorksDW2017.mdf',
+    MOVE 'AdventureWorksDW2017_log' TO '/backup/AdventureWorksDW2017_log.ldf',
+    FILE = 1,
+    NOUNLOAD,
+    STATS = 5;
+GO
